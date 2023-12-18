d5ab5cbffe5b5f75ed40948d1e04789ebaacc182
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2023-12-18 11:22:35 +0100

    Add files via upload

---
+++ ps1/extension.ps1
@@ -0,0 +1,7 @@
+﻿cls
+$ruta = Read-Host "Dime la ruta donde quieres buscar los ficheros"
+$extensión = Read-Host "Dime la extensión que buscas"
+$rutaDes = "C:\Users\Malaperra\Downloads"
+Get-ChildItem -Path $ruta -File "*.$extension" | Copy-Item -Destination $rutaDes -Force
+Write-Host "Los archivos con extensión $extensión han sido copiados a $rutaDes..."
+#El -Force sobreescribe si hay uno igual 
