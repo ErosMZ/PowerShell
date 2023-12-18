200a61f6130dfbf72d004cd69169546824207b26
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2023-12-18 16:26:06 +0100

    Update extension.ps1

--- ps1/extension.ps1
+++ ps1/extension.ps1
@@ -1,7 +1,7 @@
 ﻿Host-Clear
 $ruta = Read-Host "Dime la ruta donde quieres buscar los ficheros"
 $extensión = Read-Host "Dime la extensión que buscas"
-$rutaDes = "C:\Users\Malaperra\Downloads"
+$rutaDes = "C:\$env:userprofile\Downloads"
 Get-ChildItem -Path $ruta -File "*.$extension" | Copy-Item -Destination $rutaDes -Force
 Write-Host "Los archivos con extensión $extensión han sido copiados a $rutaDes..."
 #El -Force sobreescribe si hay uno igual 
