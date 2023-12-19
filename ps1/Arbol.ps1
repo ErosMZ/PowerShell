a8a7851433313e5ff253ab60848649cd2530d383
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2023-12-19 20:39:32 +0100

    Add files via upload

---
+++ ps1/Arbol.ps1
@@ -0,0 +1,14 @@
+﻿#triangulo
+$veces = Read-Host "Dime el tamaño"
+$array= @()
+
+for(($a = 1); $a -le $veces; $a = $a+1){
+    $array += "*"
+    for(($b = $array.Length-1);$b -ge 0; $b--){
+    Write-Host $array
+    }
+}
+
+
+
+
