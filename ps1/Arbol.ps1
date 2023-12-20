74da4ea32922aa2159fbfd79675b876e939c0f8e
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2023-12-20 13:10:31 +0100

    Update Arbol.ps1

--- ps1/Arbol.ps1
+++ ps1/Arbol.ps1
@@ -1,14 +1,20 @@
-﻿#triangulo
+#triangulo
+Clear-Host
 $veces = Read-Host "Dime el tamaño"
 $array= @()
 
-for(($a = 1); $a -le $veces; $a = $a+1){
+for(($a = 1); $a -le $veces; $a++){
     $array += "*"
-    for(($b = $array.Length-1);$b -ge 0; $b--){
-    Write-Host $array
-    }
+   Write-Host $array
 }
+Write-Host
 
+$veces1 = Read-Host "Dime el tamaño del segundo árbol"
+$array1 = @()
+$veces1 = [int]$veces1
+Write-Host
 
-
-
+for ($b = $veces1; $b -ge 1; $b--) {
+    $array1 = "* " * $b
+    Write-Host $array1
+}
